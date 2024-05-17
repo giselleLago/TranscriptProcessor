d5a1a0cad45833c1c9b8e4c09ec360f5322193a4
Giselle Lago <gisellelago25994@gmail.com>
2024-05-17 09:44:16 +0200

    Add project files.

---
+++ TranscriptsProcessor/Program.cs
@@ -0,0 +1,75 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using TranscriptsProcessor.Services;
+using TranscriptsProcessor.TranscriptionService;
+
+namespace TranscriptsProcessor
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            // Build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            // Set up Dependency Injection
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(builder =>
+                {
+                    builder.AddConfiguration(configuration.GetSection("Logging"));
+                    builder.AddConsole();
+                })
+                .BuildServiceProvider();
+
+            var host = Host.CreateDefaultBuilder(args)
+                 .ConfigureServices((hostContext, services) =>
+                 {
+                     ConfigureServices(services);
+                 })
+                 .Build();
+
+            var app = new CommandLineApplication();
+            app.HelpOption();
+
+            var pathArg = app.Argument("path", "Path to get user data").IsRequired();
+
+            app.OnExecute(() =>
+            {
+                try
+                {
+                    var services = host.Services.CreateScope().ServiceProvider;
+                    var logger = serviceProvider.GetService<ILogger<Program>>();
+                    var transcriptService = serviceProvider.GetService<ITranscriptiService>();
+                    var sender = serviceProvider.GetService<ISender>();
+                    var fileManager = serviceProvider.GetService<IFileManager>();
+
+                    var scheduler = new Scheduler(logger, fileManager, sender);
+                    scheduler.Start(pathArg.Value);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Something went wrong: " + ex.Message);
+                }
+            });
+
+            return app.Execute(args);
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<Processor>();
+            services.AddSingleton<Scheduler>();
+            services.AddSingleton<IFileValidator, FileValidator>();
+            services.AddSingleton<ISender, Sender>();
+            services.AddSingleton<IFileManager, FileManager>();
+            services.AddSingleton<ITranscriptiService, TranscriptService>();
+        }
+    }
+}
---
+++ TranscriptsProcessor/Services/Scheduler.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TranscriptsProcessor.Services
+{
+    public class Scheduler
+    {
+        public Scheduler(ILogger logger,
+                         IFileManager fileGetter,
+                         ISender senderService)
+        {
+            Logger = logger;
+            PendingFileGetter = fileGetter;
+            SenderService = senderService;
+        }
+
+        public void Start(string filePath)
+        {
+            FilePath = filePath;
+            var timeToGo = GetNextMidnight() - DateTime.Now;
+            if (timeToGo < TimeSpan.Zero)
+            {
+                timeToGo += TimeSpan.FromDays(1); // next day if it's already past midnight
+            }
+
+            timer = new Timer(timeToGo.TotalMilliseconds);
+            timer.Elapsed += (sender, args) => TimerElapsed(sender, args, timer);
+            timer.AutoReset = false; // Ensure the timer runs only once
+            timer.Start();
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e, Timer timer)
+        {
+            PerformScheduledTask();
+
+            // Reset the timer to fire again in 24 hours
+            timer.Interval = 86400000; // 24 hours in milliseconds
+            timer.Start();
+        }
+
+        private DateTime GetNextMidnight()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+
+        private Task PerformScheduledTask()
+        {
+            Console.WriteLine("Performing scheduled task at " + DateTime.Now);
+            var service = new Processor(Logger, PendingFileGetter, SenderService);
+            return service.Run(FilePath);
+        }
+
+        private Timer timer;
+        private string FilePath;
+        private readonly ILogger Logger;
+        private readonly IFileManager PendingFileGetter;
+        private readonly ISender SenderService;
+    }
+}
